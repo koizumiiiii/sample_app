912d94ed8b62ae3fc99a82b566aede13b323bd94
EC2 Default User <ec2-user@ip-172-31-26-222.us-east-2.compute.internal>
2018-07-27 06:44:58 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   validates :name,  presence: true, length: { maximum: 50 }
+  
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
 end
